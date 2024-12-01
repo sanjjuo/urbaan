92fc5f9846b0dd00dbe550d36f8cc8bbb2a76378
sanjjuo <sanjeedkoraliyil29399@gmail.com>
2024-12-01 13:21:43 +0530

    height

--- my-app/src/User/Components/Slider/Slider.jsx
+++ my-app/src/User/Components/Slider/Slider.jsx
@@ -4,14 +4,14 @@ import { Carousel } from "@material-tailwind/react";
 const UserSlider = () => {
   return (
     <Carousel transition={{ duration: 1  }} autoplay={true} className='rounded-2xl'>
-      <div className="w-full h-52 lg:h-[500px] xl:h-[450px] shadow-md">
+      <div className="w-full h-56 lg:h-[500px] xl:h-[450px] shadow-md">
         <img
           src="s2.webp"
           alt="Slide 1"
           className="h-full w-full object-cover"
         />
       </div>
-      <div className="w-full h-52 lg:h-[500px] xl:h-[450px] shadow-md">
+      <div className="w-full h-56 lg:h-[500px] xl:h-[450px] shadow-md">
         <img
           src="s1.jpg"
           alt="Slide 2"
